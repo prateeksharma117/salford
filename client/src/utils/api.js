54f58de5afe00564b791b82b77d31da4b0ea28c3
Prateek <76089649+prateeksharma117@users.noreply.github.com>
2023-10-12 19:01:25 +0530

    allfav

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -115,6 +115,7 @@ export const toFav=async(id,email,token)=>{
 }
 
 export const getAllFav=async(email,token)=>{
+    console.log(token);
     if(!token) return
     try {
         const res=await api.post(`/user/allFavResidency`,
@@ -127,7 +128,7 @@ export const getAllFav=async(email,token)=>{
             },
         },
         )
-        return res.data["favResidenciesId"]
+        return res?.data["favResidenciesId"]
     } catch (e) {
         toast.error("something went wrong while fetching favorites")
         throw e
