ce7adcc0d79b8b9c2fa0026e7a05bb3094801ce1
Prateek <76089649+prateeksharma117@users.noreply.github.com>
2023-10-13 18:14:45 +0530

    responsive

--- client/src/pages/PropertyDes/PropertyDes.jsx
+++ client/src/pages/PropertyDes/PropertyDes.jsx
@@ -79,7 +79,7 @@ const PropertyDes = () => {
                         <div className="flexColStart left">
                             <div className="flexStart head">
                                 <span className="primaryText">{data?.title}</span>
-                                <span className="orangeText" style={{ fontSize: "1.2rem"}}>
+                                <span className="orangeText" style={{ fontSize: "1.2rem", width:"30%"}}>
                                     {numeral(data?.price).format("($ 0.00 a)")}
                                 </span>
                             </div>
